3e72d88a9d53b5a9f2c76ed505f0393f1353bf99
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-02-22 14:26:34 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-const path = require ('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-    entry : "./src/index.js",
 
-    module: {
-        rules : [
-            {
-                test: /\.svg$/,
-                use: 'svg-inline-loader'
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-              },
-        ],
-    },
-    output :{
-       path : path.resolve(__dirname, 'dist'),
-       filename : "index.js" 
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html',
-    })],
-    mode: "development",
+module.exports = {
+  entry: './src/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+  mode: 'development',
 };
