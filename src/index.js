c4a70bb7494f1a737f659da30a6331996abceae4
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-02-22 14:03:14 +0530

    Update files for Linter errors
    Update files for Linter errors

--- src/index.js
+++ src/index.js
@@ -1,54 +1,34 @@
 import './style.css';
 
-let task = [
-    {
-        description : "Task1",
-        completed : false,
-        index : 1
-    },
-    {
-        description : "Task2",
-        completed : false,
-        index : 2
-    },
-    {
-        description : "Task3",
-        completed : false,
-        index : 3
-    },
-    
-]
+const task = [
+  {
+    description: 'Task1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Task2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Task3',
+    completed: false,
+    index: 3,
+  },
 
+];
 
-let list = document.querySelector('.list')
+const list = document.querySelector('.list');
 
-
-function displayData () {
-task.forEach((task,index) => {
- let id = index+1;
- let listElement = document.createElement('li');
-listElement.classList.add('listElement')
-listElement.innerHTML = '<div class = "checkbox"><input type = "checkbox" class= "taskCompleted"></div>';
-listElement.innerHTML += `<div class = "listText">${task.description}</div>`;
-listElement.innerHTML +=  '<div class = "delete"><img src="https://img.icons8.com/material-rounded/24/null/menu-2.png"/></div>'
-list.appendChild(listElement);
-
-}); 
+function displayData() {
+  task.forEach((task) => {
+    const listElement = document.createElement('li');
+    listElement.classList.add('listElement');
+    listElement.innerHTML = '<div class = "checkbox"><input type = "checkbox" class= "taskCompleted"></div>';
+    listElement.innerHTML += `<div class = "listText">${task.description}</div>`;
+    listElement.innerHTML += '<div class = "delete"><img src="https://img.icons8.com/material-rounded/24/null/menu-2.png"/></div>';
+    list.appendChild(listElement);
+  });
 }
 displayData();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
