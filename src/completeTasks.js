503bc8d8156273a7eed90dfa149d00c8cf8237f6
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-02-24 21:39:55 +0530

    Update file as per review

--- src/completeTasks.js
+++ src/completeTasks.js
@@ -1,8 +1,14 @@
 // eslint-disable-next-line
-import { tasks } from './index.js';
+import { tasks } from './crud.js';
 
 export default function completeTasks() {
   const incomplete = tasks.filter((task) => task.completed !== true);
+  tasks.length = 0;
+  let i = 1;
+  incomplete.forEach((element) => {
+    element.id = i;
+    i += 1;
+  });
   localStorage.setItem('tasks', JSON.stringify(incomplete));
   window.location.reload();
 }
