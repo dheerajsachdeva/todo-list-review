3c005cd2eaed769ec3882928464968559a9e96aa
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-02-24 18:13:12 +0530

    Update files for Linter errors

--- src/completeTasks.js
+++ src/completeTasks.js
@@ -1,7 +1,8 @@
-import {tasks} from './index.js'
+// eslint-disable-next-line
+import { tasks } from './index.js';
 
-export default function completeTasks () {
-    let incomplete = tasks.filter((task) => task.completed !== true);
-    localStorage.setItem('tasks', JSON.stringify(incomplete));
-    window.location.reload();
-  };
+export default function completeTasks() {
+  const incomplete = tasks.filter((task) => task.completed !== true);
+  localStorage.setItem('tasks', JSON.stringify(incomplete));
+  window.location.reload();
+}
